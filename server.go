09aeeb499c25bb866380e4323edd0567174c49b6
Ian Young <ian@iangreenleaf.com>
2014-12-12 22:06:33 -0800

    Format it the way Go likes it

--- server.go
+++ server.go
@@ -5,23 +5,25 @@ import "encoding/json"
 import "net/http"
 
 type Note struct {
-  Title string `json:"title"`
-  Text string `json:"text"`
+	Title string `json:"title"`
+	Text  string `json:"text"`
 }
 
 func main() {
-  m := martini.Classic()
-  m.Get("/", func() string {
-    return "Hello world!"
-  })
-  m.Get("/notes", func() (int, string) {
-    notes := []Note{
-      Note{"First note", "Get the milk"},
-      Note{"Second note", "Buy bread"},
-    }
-    js, err := json.Marshal(notes)
-    if err != nil { return 500, err.Error() }
-    return http.StatusOK, string(js)
-  })
-  m.Run()
+	m := martini.Classic()
+	m.Get("/", func() string {
+		return "Hello world!"
+	})
+	m.Get("/notes", func() (int, string) {
+		notes := []Note{
+			Note{"First note", "Get the milk"},
+			Note{"Second note", "Buy bread"},
+		}
+		js, err := json.Marshal(notes)
+		if err != nil {
+			return 500, err.Error()
+		}
+		return http.StatusOK, string(js)
+	})
+	m.Run()
 }
